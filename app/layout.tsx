b315c8e1dd8d15d8df2bef967d2eaa0944440b11
limitlessnx <newtinoski@gmail.com>
2026-07-24 18:28:28 -0400

    Load unified Fluxknight product theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./flux-theme.css";
 import SiteShell from "@/components/SiteShell";
 
 export const metadata: Metadata = {
