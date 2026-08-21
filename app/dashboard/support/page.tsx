f33d195fe802b2b7a15c44e35ebfb68bd6189555
limitlessnx <newtinoski@gmail.com>
2026-08-21 20:10:53 +0100

    Load mobile Leo chat sizing fixes

--- app/dashboard/support/page.tsx
+++ app/dashboard/support/page.tsx
@@ -1,5 +1,6 @@
 import AgentLeoClient from "./AgentLeoClient";
 import "./support.css";
+import "./leo-mobile-chat.css";
 
 export default function SupportPage() {
   return <div className="admin-page"><AgentLeoClient /></div>;
