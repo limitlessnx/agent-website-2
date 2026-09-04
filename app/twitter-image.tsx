912e4c49d761bd5fdf5ba05f617b788f01456ab0
limitlessnx <newtinoski@gmail.com>
2026-09-04 12:38:33 +0100

    Phase 15: make Twitter image metadata explicit

--- app/twitter-image.tsx
+++ app/twitter-image.tsx
@@ -1,1 +1,9 @@
-export { alt, size, contentType, default } from "./opengraph-image";
+import OpenGraphImage from "./opengraph-image";
+
+export const alt = "Fluxknight AI automation for connected customer operations";
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
+
+export default function TwitterImage() {
+  return OpenGraphImage();
+}
