4dc59a014479877deff36432b7d19918b7213997
limitlessnx <newtinoski@gmail.com>
2026-09-18 12:01:41 -0400

    Remove embedded Maia storyboard payload

--- app/case-studies/maia/context/storyboard.ts
+++
@@ -1,7 +0,0 @@
-import c0 from "./chunk0";
-import c1 from "./chunk1";
-import c2 from "./chunk2";
-import c3 from "./chunk3";
-import c4 from "./chunk4";
-
-export const maiaBatch1Storyboard = `data:image/webp;base64,${c0}${c1}${c2}${c3}${c4}`;
