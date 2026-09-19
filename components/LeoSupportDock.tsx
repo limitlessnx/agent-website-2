a8bf49c024fdf35e3e9bd885729ac9ccb5265bff
limitlessnx <newtinoski@gmail.com>
2026-09-19 08:16:58 -0400

    Defer Leo support bundle until browser idle

--- components/LeoSupportDock.tsx
+++ components/LeoSupportDock.tsx
@@ -1,7 +1,35 @@
 "use client";
 
-import PublicLeoConsultant from "@/components/PublicLeoConsultant";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+const PublicLeoConsultant = dynamic(() => import("@/components/PublicLeoConsultant"), {
+  ssr: false,
+});
 
 export default function LeoSupportDock() {
-  return <PublicLeoConsultant />;
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    const win = window as typeof window & {
+      requestIdleCallback?: (callback: IdleRequestCallback, options?: IdleRequestOptions) => number;
+      cancelIdleCallback?: (id: number) => void;
+    };
+
+    let idleId: number | undefined;
+    let timeoutId: number | undefined;
+
+    if (win.requestIdleCallback) {
+      idleId = win.requestIdleCallback(() => setReady(true), { timeout: 1800 });
+    } else {
+      timeoutId = window.setTimeout(() => setReady(true), 900);
+    }
+
+    return () => {
+      if (idleId !== undefined && win.cancelIdleCallback) win.cancelIdleCallback(idleId);
+      if (timeoutId !== undefined) window.clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return ready ? <PublicLeoConsultant /> : null;
 }
